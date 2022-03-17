196d24b93e123ec75c6f285f4129ef6ccc04b860
Patrick Kevin Mims <patrick@Patricks-Mini.hsd1.ca.comcast.net>
2022-03-16 19:25:25 -0700

    Added as an initializer

---
+++ js-backbone/nasdaq/app/views/collectionView.js
@@ -0,0 +1,25 @@
+define(['baseCollectionView'], function(BaseCollectionView) {
+    return collectionView = BaseCollectionView.extend ({
+        initialize: function() {
+            new aggregateView({
+                model: this.collection.at(0);
+            });
+
+            new dailyView({
+                model: this.collection.at(1);
+            });
+
+            new groupView({
+                model: this.collection.at(2);
+            });
+
+            new previewView({
+                model: this.collection.at(3);
+            });
+        }
+    });
+});
+
+/*
+ * collectionView -> this acts as an initializer for models loaded in from app.js
+ * */
