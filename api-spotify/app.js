400abc70953ac1a712578c289c2b865d138ca01b
Patrick Kevin Mims <patrick@Patricks-Mini.hsd1.ca.comcast.net>
2022-03-22 14:03:40 -0700

    Replacing Deprecated Libraries

--- api-spotify/app.js
+++ api-spotify/app.js
@@ -1,18 +1,18 @@
-var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
+/* DEPRECATED: request 
+ * REPLACEMENT: got
+ * var request = require('request'); // "Request" library
+ * */
+var express = require('express');
 var cors = require('cors');
-var querystring = require('querystring');
+/* DEPRECATED:  querystring
+ * REPLACEMENT:  URLSearchParams
+ * var querystring = require('querystring');
+ * */
 var cookieParser = require('cookie-parser');
 
 var client_id = 'CLIENT_ID'; // Your client id
 var client_secret = 'CLIENT_SECRET'; // Your secret
 var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
-
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
 var generateRandomString = function (length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
